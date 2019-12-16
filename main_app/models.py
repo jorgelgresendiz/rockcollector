007789b522496dc37334d0ac33b7527828df90fc
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-12-16 14:40:03 -0600

    Create rocks model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Rocks(models.Model):
+    name = models.CharField()
+    origin = models.CharField()
+    age = models.IntegerField()
+    location = models.CharField()
+
+    def __str__(self):
+        return self.name
